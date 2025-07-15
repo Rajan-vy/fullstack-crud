2c17b16f6ad7d215430255e411bbcf9fb5ef531f
Rajan Chaudhary <chaudharyrajanbee@gmail.com>
2025-07-15 18:55:13 +0545

    minor changes

--- backend/src/main/java/com/example/springbootCRUD/SpringbootCrudApplication.java
+++ backend/src/main/java/com/example/springbootCRUD/SpringbootCrudApplication.java
@@ -3,10 +3,16 @@ package com.example.springbootCRUD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SpringbootCrudApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
 		SpringApplication.run(SpringbootCrudApplication.class, args);
 	}
 
